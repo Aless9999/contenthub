7f30e1b40a3e3dd883cefcf0b4cdf19c4f9ba2ed
Als <macnigor@gmail.com>
2025-08-24 19:06:32 +0300

    initial commit

---
+++ src/test/java/org/macnigor/contenthub/ContenthubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.macnigor.contenthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContenthubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
