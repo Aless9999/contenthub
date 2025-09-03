f0443fca6c10d34f263f806731adebb64524e879
Als <macnigor@gmail.com>
2025-09-03 20:22:40 +0300

    add log to Post

--- src/main/java/org/macnigor/contenthub/services/PostService.java
+++ src/main/java/org/macnigor/contenthub/services/PostService.java
@@ -1,9 +1,9 @@
 package org.macnigor.contenthub.services;
 
+import lombok.extern.slf4j.Slf4j;
 import org.macnigor.contenthub.dto.CommentDto;
 import org.macnigor.contenthub.dto.ImageDto;
 import org.macnigor.contenthub.dto.PostDto;
-import org.macnigor.contenthub.entity.ImageModel;
 import org.macnigor.contenthub.entity.Post;
 import org.macnigor.contenthub.entity.User;
 import org.macnigor.contenthub.repositories.ImageRepository;
@@ -11,13 +11,12 @@ import org.macnigor.contenthub.repositories.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 public class PostService {
-
 
     private final PostRepository postRepository;
     private final ImageRepository imageRepository;
@@ -29,37 +28,58 @@ public class PostService {
     }
 
     public List<Post> getAllPosts() {
-        return postRepository.findAll();
+        log.debug("Запрос всех постов");
+        List<Post> posts = postRepository.findAll();
+        log.info("Найдено {} пост(ов)", posts.size());
+        return posts;
     }
 
     public Post createPost(PostDto postDto, User user) {
-        Post post = new Post();
-        post.setTitle(postDto.getTitle());
-        post.setCaption(postDto.getCaption());
-        post.setLocation(postDto.getLocation());
-        post.setUser(user);
+        log.info("Создание поста пользователем: {}", user.getUsername());
+        try {
+            Post post = new Post();
+            post.setTitle(postDto.getTitle());
+            post.setCaption(postDto.getCaption());
+            post.setLocation(postDto.getLocation());
+            post.setUser(user);
 
-        // Сохраняем сам пост
-        postRepository.save(post);
-        return post;
+            postRepository.save(post);
+
+            log.info("Пост с id={} успешно создан пользователем {}", post.getId(), user.getUsername());
+            return post;
+        } catch (Exception e) {
+            log.error("Ошибка при создании поста пользователем {}", user.getUsername(), e);
+            throw e;
+        }
     }
 
     public List<Post> getAllPostsForUser(Long userId) {
-        return getAllPosts().stream()
+        log.debug("Запрос постов для пользователя id={}", userId);
+        List<Post> userPosts = getAllPosts().stream()
                 .filter(post -> post.getUser().getId().equals(userId))
                 .collect(Collectors.toUnmodifiableList());
+
+        log.info("Найдено {} пост(ов) для пользователя id={}", userPosts.size(), userId);
+        return userPosts;
     }
 
     public Post findById(Long postId) {
-        return postRepository.findPostById(postId).orElseThrow(() -> new RuntimeException("Post with id=" + postId + "not found"));
+        log.debug("Поиск поста по id={}", postId);
+        return postRepository.findPostById(postId)
+                .orElseThrow(() -> {
+                    log.warn("Пост с id={} не найден", postId);
+                    return new RuntimeException("Post with id=" + postId + " not found");
+                });
     }
 
     public Post getPostById(Long id) {
+        log.debug("Получение поста по id={}", id);
         return findById(id);
     }
 
     public List<PostDto> getAllPostDtos() {
-        return postRepository.findAll().stream()
+        log.debug("Запрос DTO всех постов");
+        List<PostDto> postDtos = postRepository.findAll().stream()
                 .map(post -> {
                     PostDto dto = new PostDto();
                     dto.setId(post.getId());
@@ -91,7 +111,9 @@ public class PostService {
                     return dto;
                 })
                 .toList();
+
+        log.info("Сформировано {} DTO пост(ов)", postDtos.size());
+        return postDtos;
     }
 
 }
-
