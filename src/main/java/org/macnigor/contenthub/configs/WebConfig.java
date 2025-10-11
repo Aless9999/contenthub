1bf4a6a5defbaef3c2da3ea7faa86243734c9b7f
Als <macnigor@gmail.com>
2025-10-11 16:53:44 +0300

    response image from local directory

---
+++ src/main/java/org/macnigor/contenthub/configs/WebConfig.java
@@ -0,0 +1,14 @@
+package org.macnigor.contenthub.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/ImageContent/**")
+                .addResourceLocations("file:./ImageContent/");
+    }
+}
